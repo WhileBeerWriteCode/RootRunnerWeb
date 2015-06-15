0328a2c3b26f85be1fd4f303b285065cb6e6e1e8
Benjamin Daschel <coffeemaxed@gmail.com>
2015-06-14 22:43:41 -0700

    get rid of unneeded crap and customize the loading screen

--- rootrunner.js
+++ rootrunner.js
@@ -16,20 +16,24 @@ Game = {
     // The total width of the game screen. Since our grid takes up the entire screen
     //  this is just the width of a tile times the width of the grid
     width: function() {
-        return this.map_grid.width * this.map_grid.tile.width;
+        return window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth;
     },
 
     // The total height of the game screen. Since our grid takes up the entire screen
     //  this is just the height of a tile times the height of the grid
     height: function() {
-        return this.map_grid.height * this.map_grid.tile.height;
+        return window.innerHeight
+            || document.documentElement.clientHeight
+            || document.body.clientHeight;
     },
 
     // Initialize and start our game
     start: function() {
         // Start crafty and set a background color so that we can see it's working
         Crafty.init(Game.width(), Game.height());
-        Crafty.background('rgb(249, 223, 125)');
+        Crafty.background('#000');
 
         Crafty.scene('Loading');
     }
@@ -61,88 +65,9 @@ Crafty.c('Grid', {
 Crafty.c('Actor', {
     init: function() {
         this.requires('2D, Canvas, Grid');
-    },
-});
-
-Crafty.c('Tree', {
-    init: function() {
-        this.requires('Actor, Color, Solid');
-        this.color('rgb(20, 125, 40)');
-    }
-});
-
-Crafty.c('Bush', {
-    init: function() {
-        this.requires('Actor, Color, Solid');
-        this.color('rgb(20, 185, 40)');
     }
 });
 
 
-// This is the player-controlled character
-Crafty.c('PlayerCharacter', {
-    init: function() {
-        this.requires('Actor, Fourway, player_sprite, SpriteAnimation, Collision')
-            .fourway(4)
-            .bind('Moved', this.checkCollision)
-            .onHit('Village', this.visitVillage)
-            .bind('NewDirection', this.changeDirection)
 
-            /*
-                Define the player animations reels for the
-                four directions the player can walk. chr
-             */
-            .reel('player_up', 500, 0, 0, 3)
-            .reel('player_right', 500, 0, 1, 3)
-            .reel('player_down', 500, 0, 2, 3)
-            .reel('player_left', 500, 0, 3, 3)
-    },
-
-    changeDirection : function(e){
-        if(e.x > 0){
-            this.animate('player_right', -1);
-        }else if(e.x < 0){
-            this.animate('player_left', -1);
-        }else if(e.y < 0){
-            this.animate('player_up', -1);
-        }else if(e.y > 0){
-            this.animate('player_down', -1);
-        }else{
-            this.pauseAnimation();
-        }
-    },
-
-    checkCollision: function (from) {
-        /*
-            We are simply moving the player
-            back to the position they started at before the collision.
-         */
-        if(this.hit('Solid')){
-            var attrs = {};
-            attrs[from.axis] = from.oldValue;
-            this.attr(attrs);
-        }
-    },
-
-    visitVillage: function (data){
-        var village = data[0].obj;
-        village.collect();
-    }
-
-
-
-});
-
-// A village is a tile on the grid that the PC must visit in order to win the game
-Crafty.c('Village', {
-    init: function() {
-        this.requires('Actor, Color')
-            .color('rgb(170, 125, 40)');
-    },
-
-    collect: function() {
-        this.destroy();
-        Crafty.trigger('VillageVisited', this);
-    }
-});
 
